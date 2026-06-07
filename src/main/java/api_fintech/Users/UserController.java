892b461f759efc3341a8c49ce344b7182eb92f9a
Santiago <santykantor@gmail.com>
2026-06-07 18:20:08 -0300

    fix: add description and responses in create user endpoint

--- src/main/java/api_fintech/Users/UserController.java
+++ src/main/java/api_fintech/Users/UserController.java
@@ -1,6 +1,8 @@
 package api_fintech.Users;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +20,11 @@ public class UserController {
         this.userService = userService;
     }
 
-    @Operation(summary = "Crear un usuario")
+    @Operation(summary = "Crear un usuario", description = "Crea un usuario para luego poder crear una cuenta bancaria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario creadao exitosamente"),
+            @ApiResponse(responseCode = "400", description = "JSON inválido o error de validación en los datos de entrada"),
+    })
     @PostMapping("/add")
     public User createUser(@RequestBody User user) {
         return userService.AddUser(user);
